125cac4e4236287509cd6cc15f64de416e664e9b
Paul Khuong <pvk@google.com>
2019-06-23 11:18:45 -0400

    bazelify

---
+++ BUILD
@@ -0,0 +1,7 @@
+cc_library(
+    name = "csm",
+    srcs = ["csm.c"],
+    hdrs = ["csm.h"],
+    visibility = ["//visibility:public"],
+    deps = [],
+)
--- csm.h
+++ csm.h
@@ -2,9 +2,17 @@
 #define CONFIDENCE_SEQUENCE_METHOD_H
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int
 csm(double *OUT_log_level, uint64_t n, double alpha, uint64_t s, double log_eps);
 
 double
 beta_icdf(uint64_t a, uint64_t b, double alpha, int direction);
+  
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif
 #endif /* !CONFIDENCE_SEQUENCE_METHOD_H */
